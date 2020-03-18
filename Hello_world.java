9c960c2b13b9b69564076909fc5056249da997a4
Briac Andrei <andybriac@yahoo.com>
2020-03-18 22:57:52 +0200

    Initial commit in new branch

---
+++ Hello_world.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String temp = "Hello world!";
+        System.out.println(temp);
+    }
+}
